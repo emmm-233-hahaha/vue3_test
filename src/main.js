c8a50478f4d0341a5fdf142165628a007be650c4
jiangxue <2312803846@qq.com>
2022-10-01 02:16:39 +0800

    feat: 入口文件分析

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+// 不再引入Vue构造函数--import { createApp } from 'vue';
+// 而是名为createApp的构造函数
+// 构造函数要通过new关键字调用
+// 工厂函数可以直接调用不必new关键字
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue' // 引入所有组件的父组件
+
+
+createApp(App).mount('#app')// 创建应用实例对象并挂载
